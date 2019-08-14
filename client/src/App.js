5ede227a5c4abeb2d351d3097af856358cee37d7
Clint Brodar <clintbrodar@gmail.com>
2019-08-13 18:39:18 -0700

    Pages, components and api set up

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Home from './components/Home';
-import Search from './components/Search';
-import Saved from './components/Saved';
+// import Search from './components/Search';
+// import Saved from './components/Saved';
 
-class App extends Component {
-  render() {
+function App() {
     return (
       <Router>
         <Route exact path="/" component={Home} />
-        <Route exact path="/search" component={Search} />
-        <Route exact path="/saved" component={Saved} />
+        {/* <Route exact path="/search" component={Search} /> */}
+        {/* <Route exact path="/saved" component={Saved} /> */}
       </Router>
-    )
-  }
+    );
 }
+  
+
 
 export default App;
